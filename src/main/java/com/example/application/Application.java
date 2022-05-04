c55b12d4ec154f96da6576f17eb5cfae1436202e
coffeina <dubaj.andrzej@gmail.com>
2022-05-04 22:58:37 +0200

    Small change

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -17,7 +17,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
-@PWA(name = "Flow CRM Tutorial", shortName = "Flow CRM Tutorial", offlineResources = {})
+@PWA(name = "Flow CRM", shortName = "Flow CRM Tutorial", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
