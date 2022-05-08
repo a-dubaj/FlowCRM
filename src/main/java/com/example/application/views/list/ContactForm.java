7d9a3e480fd06c3997aa574f76e3bd09312cbb61
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-08 15:12:50 +0200

    Added binder

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -1,6 +1,7 @@
 package com.example.application.views.list;
 
 import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Contact;
 import com.example.application.data.entity.Status;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
@@ -10,10 +11,13 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
 
 import java.util.List;
 
 public class ContactForm extends FormLayout {
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
     TextField firstName = new TextField("First Name");
     TextField lastName = new TextField("Last Name");
     EmailField email = new EmailField("Email");
@@ -23,8 +27,10 @@ public class ContactForm extends FormLayout {
     Button save = new Button("Save");
     Button delete = new Button("Delete");
     Button close = new Button("Cancel");
+    private Contact contact;
 
     public ContactForm(List<Company> companies, List<Status> statuses) {
+        binder.bindInstanceFields(this);
         addClassName("contact-form");
         company.setItems(companies);
         company.setItemLabelGenerator(Company::getName);
@@ -34,6 +40,10 @@ public class ContactForm extends FormLayout {
 
     }
 
+    public void setContact(Contact contact){
+        this.contact = contact;
+        binder.readBean(contact);
+    }
     private HorizontalLayout createButtonLayout() {
         save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -12,7 +12,6 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-import java.util.Collections;
 
 @PageTitle("Contacts | CRM")
 @Route(value = "")
