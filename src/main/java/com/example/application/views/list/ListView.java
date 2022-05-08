668386123fd7089d52aa2c7865964e513ee06d23
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-08 15:53:17 +0200

    Update

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -54,6 +54,21 @@ public class ListView extends VerticalLayout {
     private void configureForm() {
         form = new ContactForm(service.findAllCompanies(), service.findAllStatuses());
         form.setWidth("25em");
+        form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
+    }
+
+    private void saveContact(ContactForm.SaveEvent event) {
+        service.seveContact(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        service.deleteContact(event.getContact());
+        updateList();
+        closeEditor();
     }
 
     private Component getToolbar() {
@@ -63,9 +78,15 @@ public class ListView extends VerticalLayout {
         filterText.addValueChangeListener(e -> updateList());
 
         Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(e -> addContact());
         HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
         toolbar.addClassName("toolbar");
         return toolbar;
+    }
+
+    private void addContact() {
+        grid.asMultiSelect().clear();
+        editContact(new Contact());
     }
 
     private void configureGrid() {
@@ -79,9 +100,9 @@ public class ListView extends VerticalLayout {
     }
 
     private void editContact(Contact contact) {
-        if(contact == null){
+        if (contact == null) {
             closeEditor();
-        } else{
+        } else {
             form.setContact(contact);
             form.setVisible(true);
             addClassName("editing");
