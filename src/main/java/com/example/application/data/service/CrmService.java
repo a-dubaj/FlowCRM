0dd6740c81528758ef5c4e487548f216cac5ddf6
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-08 14:48:53 +0200

    Find all companies and statuses

--- src/main/java/com/example/application/data/service/CrmService.java
+++ src/main/java/com/example/application/data/service/CrmService.java
@@ -1,6 +1,8 @@
 package com.example.application.data.service;
 
+import com.example.application.data.entity.Company;
 import com.example.application.data.entity.Contact;
+import com.example.application.data.entity.Status;
 import com.example.application.data.repository.CompanyRepository;
 import com.example.application.data.repository.ContactRepository;
 import com.example.application.data.repository.StatusRepository;
@@ -20,11 +22,36 @@ public class CrmService {
         this.statusRepository = statusRepository;
     }
 
-    public List<Contact> findAllContacts(String filterText){
-        if(filterText == null || filterText.isEmpty()){
+    public List<Contact> findAllContacts(String filterText) {
+        if (filterText == null || filterText.isEmpty()) {
             return contactRepository.findAll();
         } else {
             return contactRepository.search(filterText);
         }
     }
+
+    public long countContacts() {
+        return contactRepository.count();
+    }
+
+    public void deleteContact(Contact contact) {
+        contactRepository.delete(contact);
+    }
+
+    public void seveContact(Contact contact) {
+        if (contact == null) {
+            System.err.println("Contact is null.");
+            return;
+        }
+        contactRepository.save(contact);
+    }
+
+    public List<Company> findAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    public List<Status> findAllStatuses() {
+        return statusRepository.findAll();
+    }
+
 }
