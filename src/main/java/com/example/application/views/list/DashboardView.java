d334a1762303bc30e5b195f0b2c65b7c4703a7a1
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-08 17:36:35 +0200

    Added dasbboard with chart for contacts

--- src/main/java/com/example/application/views/list/DashboardView.java
+++ src/main/java/com/example/application/views/list/DashboardView.java
@@ -2,6 +2,11 @@ package com.example.application.views.list;
 
 import com.example.application.data.service.CrmService;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.DataSeries;
+import com.vaadin.flow.component.charts.model.DataSeriesItem;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -19,10 +24,18 @@ public class DashboardView extends VerticalLayout {
     }
 
     private Component getContactStats() {
-        return null;
+        Span stats = new Span(service.countContacts() + " contact");
+        stats.addClassNames("text-xl", "mt-m");
+        return stats;
     }
 
     private Component getCompaniesChart() {
-        return null;
+        Chart chart = new Chart(ChartType.PIE);
+        DataSeries dataSeries = new DataSeries();
+        service.findAllCompanies().forEach(company -> {
+            dataSeries.add(new DataSeriesItem(company.getName(), company.getEmployeeCount()));
+        });
+        chart.getConfiguration().setSeries(dataSeries);
+        return chart;
     }
 }
