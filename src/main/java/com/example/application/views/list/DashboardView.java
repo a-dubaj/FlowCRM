2547a5f8bacd51beaeee74627d9d52db6e8d753d
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-08 17:15:12 +0200

    Added dashboard view

---
+++ src/main/java/com/example/application/views/list/DashboardView.java
@@ -0,0 +1,28 @@
+package com.example.application.views.list;
+
+import com.example.application.data.service.CrmService;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "dashboard", layout = MainLayout.class)
+@PageTitle("Dashboard CRM")
+public class DashboardView extends VerticalLayout {
+    private CrmService service;
+
+    public DashboardView(CrmService service) {
+        this.service = service;
+        addClassName("dashboard-view");
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        add(getContactStats(), getCompaniesChart());
+    }
+
+    private Component getContactStats() {
+        return null;
+    }
+
+    private Component getCompaniesChart() {
+        return null;
+    }
+}
