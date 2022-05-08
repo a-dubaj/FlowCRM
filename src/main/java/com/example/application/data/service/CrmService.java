ddd191467154290ef199b6c739b50ed2c90cd55f
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-08 13:56:32 +0200

    Added Service and Filtring

--- src/main/java/com/example/application/data/service/CrmService.java
+++ src/main/java/com/example/application/data/service/CrmService.java
@@ -1,4 +1,30 @@
 package com.example.application.data.service;
 
+import com.example.application.data.entity.Contact;
+import com.example.application.data.repository.CompanyRepository;
+import com.example.application.data.repository.ContactRepository;
+import com.example.application.data.repository.StatusRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CrmService {
+    private final ContactRepository contactRepository;
+    private final CompanyRepository companyRepository;
+    private final StatusRepository statusRepository;
+
+    public CrmService(ContactRepository contactRepository, CompanyRepository companyRepository, StatusRepository statusRepository) {
+        this.contactRepository = contactRepository;
+        this.companyRepository = companyRepository;
+        this.statusRepository = statusRepository;
+    }
+
+    public List<Contact> findAllContacts(String filterText){
+        if(filterText == null || filterText.isEmpty()){
+            return contactRepository.findAll();
+        } else {
+            return contactRepository.search(filterText);
+        }
+    }
 }
