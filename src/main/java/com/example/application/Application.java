346f2a00a7177fa9f81bba56a2d80f37eefb3762
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-09 16:20:11 +0200

    Added offline mode

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -10,10 +10,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
-@PWA(name = "CRM Application", shortName = "CRM", offlineResources = {"images/logo.png"})
+@PWA(
+        name = "CRM Application",
+        shortName = "CRM",
+        offlinePath = "offline.html",
+        offlineResources = {"images/logo.png", "images/offline.png"})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
