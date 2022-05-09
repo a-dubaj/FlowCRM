c9895da8d2a60a8ae95877018edfef08d1013262
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-09 19:32:51 +0200

    Added integration test

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -11,10 +11,12 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.context.annotation.Scope;
 
 import javax.annotation.security.PermitAll;
 
-
+@org.springframework.stereotype.Component
+@Scope("prototype")
 @PageTitle("Contacts | CRM")
 @Route(value = "", layout = MainLayout.class)
 @PermitAll
