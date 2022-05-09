b3ac2003e3d1839798444e16dc6817524407ae99
a-dubaj <dubaj.andrzej@gmail.com>
2022-05-09 17:55:18 +0200

    Added first test

---
+++ src/test/java/com/example/application/views/list/ContactFormTest.java
@@ -0,0 +1,60 @@
+package com.example.application.views.list;
+
+import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Contact;
+import com.example.application.data.entity.Status;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactFormTest {
+    private List<Company> companies;
+    private List<Status> statuses;
+    private Contact marcUsher;
+    private Company company1;
+    private Company company2;
+    private Status status1;
+    private Status status2;
+
+    @Before
+    public void setupData() {
+        companies = new ArrayList<>();
+        company1 = new Company();
+        company1.setName("Vaadin Ltd");
+        company2 = new Company();
+        company2.setName("IT Mill");
+        companies.add(company1);
+        companies.add(company2);
+
+        statuses = new ArrayList<>();
+        status1 = new Status();
+        status1.setName("Status 1");
+        status2 = new Status();
+        status2.setName("Status 2");
+        statuses.add(status1);
+        statuses.add(status2);
+
+        marcUsher = new Contact();
+        marcUsher.setFirstName("Marc");
+        marcUsher.setLastName("Usher");
+        marcUsher.setEmail("marc@usher.com");
+        marcUsher.setStatus(status1);
+        marcUsher.setCompany(company2);
+    }
+
+    @Test
+    public void formFieldsPopulated() {
+        ContactForm form = new ContactForm(companies, statuses);
+        form.setContact(marcUsher);
+
+        Assert.assertEquals("Marc", form.firstName.getValue());
+        Assert.assertEquals("Usher", form.lastName.getValue());
+        Assert.assertEquals("marc@usher.com", form.email.getValue());
+        Assert.assertEquals(company2, form.company.getValue());
+        Assert.assertEquals(status1, form.status.getValue());
+    }
+
+}
